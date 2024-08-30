34323c21fc08c11f504ed527d52f3855fdd420a4
Destcable <artmobpavlov21@gmail.com>
2024-08-30 15:18:08 +0300

    [upd] add .env and add lib dotenv

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 import express from 'express'
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
+
 
 app.get('/', (req, res) => { 
     res.send('Hello World')
